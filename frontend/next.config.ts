cc33c6d01dcd3bce5f180e9f7493688c58975522
MUKKU BUNNY <mukkubunny2005@gmail.com>
2026-09-13 11:43:10 +0530

    Merge remote main with local project

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "localhost:3000",
+    "127.0.0.1:3000",
+  ],
 };
 
 export default nextConfig;
